b465a0387965a7db60a59b306c5965eab12a9612
Muhammed Nufais <muhammednufais@Muhammeds-MacBook-Pro.local>
2024-07-05 16:41:30 +0530

    import fix

--- src/components/ui/darkMode.tsx
+++ src/components/ui/darkMode.tsx
@@ -11,7 +11,7 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "./dropdown-menu"
 
 export function ModeToggle() {
   const { setTheme } = useTheme()
